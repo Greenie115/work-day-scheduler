ee824d5419a94019061fc75d99a15066b4b33b58
Greenie115 <dggreen545@gmail.com>
2023-11-10 19:35:26 +0000

    formatted the time layout

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,16 @@ var dateAndTimeP = document.getElementById("currentDay")
 
 function updateClock(){
     var currentDate = dayjs();
-    var formattedDate = currentDate.format('YYYY-MM-DD HH:mm:ss');
+    var formattedDate = currentDate.format('dddd-d-MMM-YYYY HH:mm:ss');
    
     dateAndTimeP.textContent = formattedDate; 
 }
 
 function clock(){
     var currentHour = dayjs().hour()
-    console.log(currentHour)
 }
 
 updateClock();
 setInterval(updateClock, 1000)
 clock();
+
