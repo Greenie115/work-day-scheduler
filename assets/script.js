af3b93bfccf02fb20916e7d729a09c756c2ed151
Greenie115 <dggreen545@gmail.com>
2023-11-10 19:55:48 +0000

    psudocode added for tomorrow to improve workflow

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//clock function
 var dateAndTimeP = document.getElementById("currentDay")
 
 function updateClock(){
@@ -14,4 +15,13 @@ function clock(){
 updateClock();
 setInterval(updateClock, 1000)
 clock();
+// ======== END =========
 
+//1 - If the time has passed, make the to do form grey
+// ELSE IF the time is now, make the input form red
+// ELSE, the time is in the future, make the input box green
+
+// Add a switch case for each option
+// SET ATTRIBUTE of each input box based on the condition
+
+// ON clock, the save btn will store the DATASET to LOCAL STORAGE
