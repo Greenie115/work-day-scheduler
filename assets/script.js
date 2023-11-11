3cfeacdb4789794e7e361335fc3bc54e6b618eb0
Greenie115 <dggreen545@gmail.com>
2023-11-11 17:40:13 +0000

    added variables for each of the <span> tags to target them

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,19 @@
 //clock function
 var dateAndTimeP = document.getElementById("currentDay")
+var currentHour = dayjs().format('HH')
+var scheduleHour9 = document.querySelector('#workHour9').innerText
+var scheduleHour10 = document.querySelector('#workHour10').innerText
+var scheduleHour11 = document.querySelector('#workHour11').innerText
+var scheduleHour12 = document.querySelector('#workHour12').innerText
+var scheduleHour13 = document.querySelector('#workHour13').innerText
+var scheduleHour14 = document.querySelector('#workHour14').innerText
+var scheduleHour15 = document.querySelector('#workHour15').innerText
+var scheduleHour16 = document.querySelector('#workHour16').innerText
+var scheduleHour17 = document.querySelector('#workHour17').innerText
+var inputBox = document.querySelectorAll('.input')
+
+var allHours = [scheduleHour9, scheduleHour10, scheduleHour11, scheduleHour12, scheduleHour13, scheduleHour14, scheduleHour15, scheduleHour16, scheduleHour17]
+console.log(inputBox.setAttribute("class", "past"))
 
 function updateClock(){
     var currentDate = dayjs();
@@ -7,21 +21,27 @@ function updateClock(){
    
     dateAndTimeP.textContent = formattedDate; 
 }
-
-function clock(){
-    var currentHour = dayjs().hour()
-}
-
 updateClock();
 setInterval(updateClock, 1000)
-clock();
+// clock();
 // ======== END =========
 
 //1 - If the time has passed, make the to do form grey
+for(i = 8; currentHour; i++){
+    if(currentHour == allHours[i]){
+        inputBox.setAttribute("class", "present")
+        }else if(currentHour < allHours[i]) {
+            inputBox.setAttribute("class", "future")
+        } else {
+            inputBox.setAttribute("class","past")
+    }
+}
+
+
 // ELSE IF the time is now, make the input form red
 // ELSE, the time is in the future, make the input box green
 
 // Add a switch case for each option
 // SET ATTRIBUTE of each input box based on the condition
 
-// ON clock, the save btn will store the DATASET to LOCAL STORAGE
+// ON click, the save btn will store the DATASET to LOCAL STORAGE
